2102a27b36939a6a01c925eac6136a56e2255589
Alvaro Martin Muñoz <alvmarmun1@alum.us.es>
2024-06-19 19:56:39 +0200

    COMMIT INICIAL

---
+++ Acme-Sf-24.5.0/src/main/java/acme/entities/projects/UserStory.java
@@ -0,0 +1,60 @@
+
+package acme.entities.projects;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.roles.Manager;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserStory extends AbstractEntity {
+
+	// Serialisation identifier ---------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@PositiveOrZero
+	private int					estimatedCost;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				acceptanceCriteria;
+
+	@NotNull
+	private Priority			priority;
+
+	private boolean				published			= false;
+
+	@URL
+	@Length(max = 255)
+	private String				url;
+
+	// Relationships ----------------------------------------------------------
+
+	@ManyToOne(optional = false)
+	@Valid
+	private Manager				manager;
+
+}
